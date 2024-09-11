621619d8238c48483da9c5df8d2d8955be15edef
NaturallyIdiotic <trevorbercher@gmail.com>
2024-09-11 15:44:26 -0400

    Learning version control in github
    Utilizing simple hello world c

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -3,5 +3,7 @@ public class HelloWorldGit
    public static void main(String[] args)
    {
       System.out.println("Hello World");
+      
+      System.out.println("Program modified, but able to revert to original version using github desktop");
    }
 }
